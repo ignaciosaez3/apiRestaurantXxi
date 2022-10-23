b44239030b46306fcc97cd6649c0d9176a77a9bc
Ignacio Saez <ignaciosaezreyes3@gmail.com>
2022-10-23 03:09:17 -0300

    Se agrego el agregar usuario y mesa

--- src/main/java/cl/apirestaurantxxi/ApiRestaurantXxiApplication.java
+++ src/main/java/cl/apirestaurantxxi/ApiRestaurantXxiApplication.java
@@ -3,6 +3,9 @@ package cl.apirestaurantxxi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +13,24 @@ public class ApiRestaurantXxiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiRestaurantXxiApplication.class, args);
+
+
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowCredentials(false)
+                        .allowedHeaders("*")
+                        .exposedHeaders("*")
+                        .maxAge(60 * 30)
+                        .allowedMethods("*");
+            }
+        };
     }
 
 }
